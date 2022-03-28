0969d325dc525d35e08c81ec6407d15a786e7c95
mariosamedlola <mariosamedlola@gmail.com>
2022-03-28 11:53:42 +0800

    penambahan navbar dan scroll js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// event pada saat link di klik
+$(".page-scroll").on("click", function (e) {
+  // ambil isi href
+  var tujuan = $(this).attr("href");
+  //tangkap elemen ybs
+  var elemenTujuan = $(tujuan);
+
+  // pindahan scroll
+  $("html").animate(
+    {
+      scrollTop: elemenTujuan.offset().top - 50,
+    },
+    1500,
+    "easeInOutExpo"
+  );
+
+  e.preventDefault();
+});
