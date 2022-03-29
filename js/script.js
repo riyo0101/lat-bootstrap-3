eac894a18259be9b7a70783eccc6d116595ad579
mariosamedlola <mariosamedlola@gmail.com>
2022-03-29 15:51:07 +0800

    Penambahan Efeck Parallax di jumbotron about & portfolio.

--- js/script.js
+++ js/script.js
@@ -16,3 +16,37 @@ $(".page-scroll").on("click", function (e) {
 
   e.preventDefault();
 });
+
+// parallax
+//about
+$(window).on("load", function () {
+  $(".pKiri").addClass("pMuncul");
+});
+
+$(window).on("load", function () {
+  $(".pKanan").addClass("pMuncul");
+});
+
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+
+  // jumbotron
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + wScroll / 6 + "%)",
+  });
+  $(".jumbotron h1").css({
+    transform: "translateS(0px, " + wScroll / 2 + "%)",
+  });
+  $(".jumbotron p").css({
+    transform: "translate(0px, " + wScroll / 1.8 + "%)",
+  });
+
+  // portfolio
+  if (wScroll > $(".portfolio").offset().top - 500) {
+    $(".portfolio .thumbnail").each(function (i) {
+      setTimeout(function () {
+        $(".portfolio .thumbnail").eq(i).addClass("muncul");
+      }, 400 * (i + 1));
+    });
+  }
+});
